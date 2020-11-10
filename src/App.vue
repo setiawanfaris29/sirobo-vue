15c64a1383c11be698070813b76737e3ce15a7cf
M.Faris Setiawan <setiawanfarism29@gmail.com>
2020-11-10 11:07:29 +0700

    UI Card

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
   <div id="app">
-    <Test/>
+    <Test />
   </div>
 </template>
 
 <script>
-import Test from './components/Test.vue'
+import Test from "./components/Test.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Test
-  }
-}
+    Test,
+  },
+};
 </script>
 
 <style>
-body{
+body {
   background-color: #eeeeee;
   display: grid;
   grid-template-rows: auto;
   justify-content: center;
   align-items: center;
-  }
+}
 
-body, html {
-   margin: 0;
-   height: 100%;
-  }
-
-
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
 </style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,39 +1,63 @@
 <template>
-<div class="holder">
+<div container>
 
-  <row container gutter="12">
-      <column xs="12">
-        <button> Pendafataran Anggota Baru </button>
-      </column>
-      <column xs="12">
-        <button > Coworking Space </button>
-      </column>
+
+<row gutter="50">
+
+    <column xs="12" md="4" lg="3">
+      <div class="card" style="background-color:#68b0ab;" @click="showModal">
+        Pendafataran Anggota Baru
+      </div>
+    </column>
+
+    <column xs="12" md="4" lg="3">
+      <div class="card" style="background-color:#8fc0a9;">
+        Coworking Space
+      </div>
+    </column>
+
+    <column xs="12" md="4" lg="3">
+      <div class="card" style="background-color:#c8d5b9;">
+        Buku
+      </div>
+    </column>
+
+    <column xs="12" md="4" lg="3">
+      <div class="card" style="background-color:#faf3dd;">
+        Anak-anak
+      </div>
+    </column>
+
   </row>
-
- <row container gutter="12">
-      <column xs="8" >
-       <button > Buku </button>
-      </column>
-      <column xs="8">
-       <button > Anak-anak </button>
-      </column>
-  </row>
-
-
+  
 </div>
-    
+  
 </template>
 
 <script>
 export default {
-  name: 'Test',
-  props: {
-
-  }
-}
+  name: "Test",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-   
+.card {
+  display: grid;
+  width: auto;
+  height: 100%;
+  padding: 50px;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px; /* 5px rounded corners */
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+
 </style>
